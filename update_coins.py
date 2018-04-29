dc34be11b38e56323e672a6b64836102066d265c
Banca <38743943+techbanca@users.noreply.github.com>
2018-04-29 16:11:50 -0700

    Add files via upload

---
+++ update_coins.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 31 13:11:48 2016
+
+@author: coin-ai
+"""
+
+from datalib.datalib import Connection 
+from datalib.coin import Coin
+from datetime import datetime 
+from analytics.time_series import get_frequency
+
+
+db = Connection()
+COINS = db.get_all_coins()
+table = db.get_table("COIN")
+
+for COIN in COINS:
+    # print(COIN.Name)
+    ts = COIN.Performance
+    T = len(ts)
+    mx = int(max(ts.index).timestamp())
+    mn = int(min(ts.index).timestamp())
+    item = COIN.to_db_item()
+    item['MaxDate'] = mx
+    item['MinDate'] = mn
+    item['Frequency'] = get_frequency(ts)
+    item['T'] = T
+    table.put_item(Item=item)
+    # print("COIN updated: %s" % COIN.Name)
+
+    
+    
---
+++ wsgi.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import os
+import datetime
+
+# Create an application instance that web servers can use. We store it as
+# "application" (the wsgi default) and also the much shorter and convenient
+# "app".
+
+def create_app():
+
+  from run_banca import application
+  return application
+
+application = app = create_app()
+
+
+@app.context_processor
+def template_extras():
+
+    return {'enumerate': enumerate, 'len': len, 'datetime': datetime}
+
+if __name__ == '__main__':
+    application.run()
