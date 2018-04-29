ec06f3d4e01d0b460ecd1db91fd9164ab6b77250
Banca <38743943+techbanca@users.noreply.github.com>
2018-04-29 16:16:00 -0700

    Add files via upload

---
+++ tables/create-tables.sh
@@ -0,0 +1,11 @@
+aws dynamodb create-table --cli-input-json file://./FactorModel.json
+aws dynamodb create-table --cli-input-json file://./Company.json
+aws dynamodb create-table --cli-input-json file://./RegimeModel.json
+aws dynamodb create-table --cli-input-json file://./Portfolio.json
+aws dynamodb create-table --cli-input-json file://./Users.json
+aws dynamodb create-table --cli-input-json file://./Coin.json
+aws dynamodb create-table --cli-input-json file://./ReferenceData.json
+aws dynamodb create-table --cli-input-json file://./RiskFactor.json
+aws dynamodb create-table --cli-input-json file://./PerformanceData.json
+aws dynamodb create-table --cli-input-json file://./Session.json
+
---
+++ tables/drop-tables.sh
@@ -0,0 +1,12 @@
+aws dynamodb delete-table --table-name FactorModel
+aws dynamodb delete-table --table-name Company
+aws dynamodb delete-table --table-name RegimeModel
+aws dynamodb delete-table --table-name Portfolio
+aws dynamodb delete-table --table-name Users
+aws dynamodb delete-table --table-name Coin
+aws dynamodb delete-table --table-name ReferenceData
+aws dynamodb delete-table --table-name RiskFactor
+aws dynamodb delete-table --table-name PerformanceData
+aws dynamodb delete-table --table-name Session
+
+
