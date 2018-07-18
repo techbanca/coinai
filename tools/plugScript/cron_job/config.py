e0fe37e3eaad457fcb55e466c3a49f8ee4363343
Banca <38743943+techbanca@users.noreply.github.com>
2018-07-17 21:20:22 -0700

    Create config.py

---
+++ tools/plugScript/cron_job/config.py
@@ -0,0 +1,26 @@
+
+scheduler_hour_time = {
+    "month":"*",
+    "day":"*",
+    "hour":"*",
+    "minute":"15,45",
+    "second":"1"
+}
+
+scheduler_day_time = {
+    "month":"*",
+    "day":"*",
+    "hour":"0",
+    "minute":"5",
+    "second":"1"
+}
+
+scheduler_minute_time = {
+    "month":"*",
+    "day":"*",
+    "hour":"*",
+    "minute":"0,10,20,30,40,50",
+    "second":"1"
+}
+
+timezone_local = ""
