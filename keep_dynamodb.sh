58818d73eca21d495a460d03e7392e67416f943c
BancaOfficial <35514940+BancaOfficial@users.noreply.github.com>
2018-07-19 10:23:46 -0700

    Update keep_dynamodb.sh

--- keep_dynamodb.sh
+++ keep_dynamodb.sh
@@ -13,12 +13,14 @@ num=1
 iNum=1
 echo $$
 
+
 while(( $num < 5 ))
 do
     java_cn0=`ps -ef|grep java |grep DynamoDBLocal_lib |grep 8000| grep -v grep |awk '{print $2}'`
     echo $java_cn0
     if [ "${java_cn0}" = "" ]
     then
+    
         let "iNum++"
         echo $iNum
         sudo nohup java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -port 8000 -dbPath ./dbase/  &
