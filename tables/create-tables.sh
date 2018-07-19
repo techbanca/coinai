4b0484d20d9f27221a44f7d429dc230bf92093d4
chrisbanca <39719719+chrisbanca@users.noreply.github.com>
2018-07-18 18:44:31 -0700

    Update create-tables.sh

--- tables/create-tables.sh
+++ tables/create-tables.sh
@@ -3,9 +3,13 @@ aws dynamodb create-table --cli-input-json file://./Company.json
 aws dynamodb create-table --cli-input-json file://./RegimeModel.json
 aws dynamodb create-table --cli-input-json file://./Portfolio.json
 aws dynamodb create-table --cli-input-json file://./Users.json
+
+
 aws dynamodb create-table --cli-input-json file://./Coin.json
 aws dynamodb create-table --cli-input-json file://./ReferenceData.json
 aws dynamodb create-table --cli-input-json file://./RiskFactor.json
 aws dynamodb create-table --cli-input-json file://./PerformanceData.json
 aws dynamodb create-table --cli-input-json file://./Session.json
+
+
 
